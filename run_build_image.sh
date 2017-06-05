9c66594e9fbca798bc8109caf16564ffbdcfa957
Jon Jagger <jon@jaggersoft.com>
2017-06-05 22:05:06 +0100

    try removing volume mount of docker-socket seeing as docker is already installed inside the image_builder image

--- run_build_image.sh
+++ run_build_image.sh
@@ -12,20 +12,25 @@ build_image()
   if [ -d ${my_dir}/start_point ]; then
     local start_point=${my_dir}/start_point:/start_point:ro
   fi
+  #--volume=${docker_socket} \
   docker run \
     --rm \
     -it \
-    --volume=${docker_socket} \
     --volume=${docker_src} \
     --volume=${start_point} \
     cyberdojofoundation/image_builder \
       ./build_image.rb ${repo_name}
 }
 
-ls -al /var/run
-ls -al /run
-find / -name "docker.dock"
+# ls -al /var/run
+# lrwxrwxrwx 1 root root 4 Nov 30  2016 /var/run -> /run
 
+# ls -al /run
+# srw-rw----  1 root       docker        0 Jun  5 20:58 docker.sock
+
+#find / -name "docker.dock"
+
+whoami
 build_image ${1}
 exit_status=$?
 echo "exit_status=${exit_status}"
