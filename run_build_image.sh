f2ed3207e15a8011fdc7f308eee38d9fc36d839c
Jon Jagger <jon@jaggersoft.com>
2017-06-05 22:08:33 +0100

    problem seems to be space being inserted after --volume so going back to previous way of repeating --volume inside each local var

--- run_build_image.sh
+++ run_build_image.sh
@@ -7,17 +7,17 @@ build_image()
   local repo_name=${1}
   local docker_socket=/var/run/docker.sock:/var/run/docker.sock
   if [ -d ${my_dir}/docker ]; then
-    local docker_src=${my_dir}/docker:/docker:ro
+    local volume_docker_src=--volume=${my_dir}/docker:/docker:ro
   fi
   if [ -d ${my_dir}/start_point ]; then
-    local start_point=${my_dir}/start_point:/start_point:ro
+    local volume_start_point=--volume=${my_dir}/start_point:/start_point:ro
   fi
   #--volume=${docker_socket} \
   docker run \
     --rm \
     -it \
-    --volume=${docker_src} \
-    --volume=${start_point} \
+    ${volume_docker_src} \
+    ${volume_start_point} \
     cyberdojofoundation/image_builder \
       ./build_image.rb ${repo_name}
 }
