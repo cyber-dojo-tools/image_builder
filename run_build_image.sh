f925e102e29551ea4ce807a1949c9af62d97b960
Jon Jagger <jon@jaggersoft.com>
2017-06-05 07:51:22 +0100

    made repo_name a parameter

--- run_build_image.sh
+++ run_build_image.sh
@@ -4,12 +4,11 @@ my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
 build_image()
 {
+  local repo_name=${1}
+
   local org_name=cyberdojofoundation
   local tag_name=$(basename ${my_dir}) # image_builder
   local name=${org_name}/${tag_name}
-
-  local repo_name=https://github.com/cyber-dojo-languages/elm-test
-  #local repo_name=${TRAVIS_REPO_SLUG}
 
   if [ -d ${my_dir}/docker ]; then
     local docker_volume=--volume=${my_dir}/docker:/docker:ro
@@ -26,6 +25,6 @@ build_image()
     ${name} ./build_image.rb ${repo_name}
 }
 
-build_image
+build_image ${1}
 exit_status=$?
 echo "exit_status=${exit_status}"
