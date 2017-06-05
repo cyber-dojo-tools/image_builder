0280ca6cfa2764ecf8a863059aab8c22342dc721
Jon Jagger <jon@jaggersoft.com>
2017-06-05 22:22:29 +0100

    add back volume-mount of docker-socket

--- run_build_image.sh
+++ run_build_image.sh
@@ -5,17 +5,18 @@ my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 build_image()
 {
   local repo_name=${1}
-  local docker_socket=/var/run/docker.sock:/var/run/docker.sock
+  local volume_docker_socket=--volume=/var/run/docker.sock:/var/run/docker.sock
   if [ -d ${my_dir}/docker ]; then
     local volume_docker_src=--volume=${my_dir}/docker:/docker:ro
   fi
   if [ -d ${my_dir}/start_point ]; then
     local volume_start_point=--volume=${my_dir}/start_point:/start_point:ro
   fi
-  #--volume=${docker_socket} \
+
   docker run \
     --rm \
     -it \
+    ${volume_docker_socket} \
     ${volume_docker_src} \
     ${volume_start_point} \
     cyberdojofoundation/image_builder \
@@ -30,7 +31,6 @@ build_image()
 
 #find / -name "docker.dock"
 
-whoami
 build_image ${1}
 exit_status=$?
 echo "exit_status=${exit_status}"
