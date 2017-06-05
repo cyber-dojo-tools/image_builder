87a54eadf50727249549352f51bcf0547b771b72
Jon Jagger <jon@jaggersoft.com>
2017-06-05 21:25:11 +0100

    added volume for docker-socket to sh file, build image in rb file inside image_builder

--- build_image.rb
+++ build_image.rb
@@ -15,20 +15,19 @@ def from
 end
 
 def base_language_repo?
-  File.exists?('/docker/image_name.sh') &&
+  File.exists?('/docker/manifest.json') &&
   !Dir.exists?('/start_point')
 end
 
 def language_plus_test_repo?
-  !File.exists?('/docker/image_name.sh') &&
+  !File.exists?('/docker/manifest.json') &&
   Dir.exists?('/start_point')
 end
 
 def image_name
   if base_language_repo?
-    lines = IO.read('/docker/image_name.sh').split("\n")
-    line = lines.find { |line| line.start_with? 'image_name=' }
-    return line.split('=')[1].strip
+    manifest = JSON.parse(IO.read('/docker/manifest.json'))
+    return manifest['image_name']
   end
   if language_plus_test_repo?
     manifest = JSON.parse(IO.read('/start_point/manifest.json'))
@@ -37,19 +36,29 @@ def image_name
   return nil
 end
 
-# 1st step. Verify dependency settings
-
-status = dependencies.include?([ repo_name, from, image_name ])
-unless status
-  lines = [
-    'ERROR: cannot find dependency entry for',
-    "  repo_name:#{repo_name}",
-    "  from:#{from}",
-    "  image_name:#{image_name}"
-  ]
-  lines.each { |line| STDERR.puts line }
-  exit status
+def verify_dependency_settings
+  status = dependencies.include?([ repo_name, from, image_name ])
+  unless status
+    lines = [
+      'ERROR: cannot find dependency entry for',
+      "  repo_name:#{repo_name}",
+      "  from:#{from}",
+      "  image_name:#{image_name}"
+    ]
+    lines.each { |line| STDERR.puts line }
+    exit status
+  end
 end
+
+def build_the_image
+  command = "cd /docker && docker build --tag #{image_name} ."
+  system(command)
+  status = $?.exitstatus
+  puts "exit_status of [docker build] == #{status}"
+end
+
+verify_dependency_settings
+build_the_image
 
 
 
--- run_build_image.sh
+++ run_build_image.sh
@@ -5,19 +5,19 @@ my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 build_image()
 {
   local repo_name=${1}
-
+  local docker_socket=/var/run/docker.sock:/var/run/docker.sock
   if [ -d ${my_dir}/docker ]; then
-    local docker_volume=--volume=${my_dir}/docker:/docker:ro
+    local docker_src=${my_dir}/docker:/docker:ro
   fi
   if [ -d ${my_dir}/start_point ]; then
-    local start_point_volume=--volume=${my_dir}/start_point:/start_point:ro
+    local start_point=${my_dir}/start_point:/start_point:ro
   fi
-
   docker run \
     --rm \
     -it \
-    ${docker_volume} \
-    ${start_point_volume} \
+    --volume=${docker_socket} \
+    --volume=${docker_src} \
+    --volume=${start_point} \
     cyberdojofoundation/image_builder \
       ./build_image.rb ${repo_name}
 }
