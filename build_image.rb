37097ff61d959549b444a0e42f4a00ccb858e4b5
Jon Jagger <jon@jaggersoft.com>
2017-06-06 05:59:47 +0100

    pass env-vars into image_builder

--- build_image.rb
+++ build_image.rb
@@ -7,6 +7,14 @@ def repo_name
   ARGV[0]
 end
 
+def docker_username
+  ARGV[1]
+end
+
+def docker_password
+  ARGV[2]
+end
+
 def from
   dockerfile = IO.read('/docker/Dockerfile')
   lines = dockerfile.split("\n")
@@ -16,7 +24,7 @@ end
 
 def base_language_repo?
   File.exists?('/docker/manifest.json') &&
-  !Dir.exists?('/start_point')
+    !Dir.exists?('/start_point')
 end
 
 def language_plus_test_repo?
@@ -26,39 +34,64 @@ end
 
 def image_name
   if base_language_repo?
-    manifest = JSON.parse(IO.read('/docker/manifest.json'))
-    return manifest['image_name']
+    manifest = IO.read('/docker/manifest.json')
+    json = JSON.parse(manifest)
+    return json['image_name']
   end
   if language_plus_test_repo?
-    manifest = JSON.parse(IO.read('/start_point/manifest.json'))
-    return manifest['image_name']
+    manifest = IO.read('/start_point/manifest.json')
+    json = JSON.parse(manifest)
+    return json['image_name']
   end
   return nil
+end
+
+def print_diagnostic(lines)
+  lines.each { |line| STDERR.puts line }
 end
 
 def verify_dependency_settings
   status = dependencies.include?([ repo_name, from, image_name ])
   unless status
-    lines = [
+    print_diagnostic [
       'ERROR: cannot find dependency entry for',
       "  repo_name:#{repo_name}",
       "  from:#{from}",
       "  image_name:#{image_name}"
     ]
-    lines.each { |line| STDERR.puts line }
+    exit status
+  end
+end
+
+def assert_system(command)
+  system(command)
+  status = $?.exitstatus
+  unless status == 0
+    print_diagnostic [
+      "FAILED:command:#{command}",
+      "exit_status == #{status}"
+    ]
     exit status
   end
 end
 
 def build_the_image
-  command = "cd /docker && docker build --tag #{image_name} ."
-  system(command)
-  status = $?.exitstatus
-  puts "exit_status of [docker build] == #{status}"
+  assert_system "cd /docker && docker build --tag #{image_name} ."
+end
+
+def push_the_image
+  command = [
+    'docker login',
+    "--username #{docker_username}",
+    "--password #{docker_password}"
+  ].join(' ')
+  assert_system command
+  assert_system "docker push #{image_name}"
+  assert_system 'docker logout'
 end
 
 verify_dependency_settings
 build_the_image
-
+push_the_image
 
 
--- run_build_image.sh
+++ run_build_image.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-my_dir="$( cd "$( dirname "${0}" )" && pwd )"
+exit_fail()
+{
+  echo "FAILED:${1}"
+  exit 1
+}
 
 build_image()
 {
-  local repo_name=${1}
-  local volume_docker_socket=--volume=/var/run/docker.sock:/var/run/docker.sock
-  if [ -d ${my_dir}/docker ]; then
-    local volume_docker_src=--volume=${my_dir}/docker:/docker:ro
+  local work_dir=$1
+  shift
+
+  if [ ! -d ${work_dir}/docker ]; then
+    exit_fail "there is no docker/ directory"
   fi
-  if [ -d ${my_dir}/start_point ]; then
-    local volume_start_point=--volume=${my_dir}/start_point:/start_point:ro
+
+  # docker.sock is needed is you are running on a local Docker Toolbox
+  local volume_docker_socket=--volume=/var/run/docker.sock:/var/run/docker.sock
+  local volume_docker_src=--volume=${work_dir}/docker:/docker:ro
+  if [ -d ${work_dir}/start_point ]; then
+    local volume_start_point=--volume=${work_dir}/start_point:/start_point:ro
   fi
 
   docker run \
@@ -20,11 +29,7 @@ build_image()
     ${volume_docker_src} \
     ${volume_start_point} \
     cyberdojofoundation/image_builder \
-      ./build_image.rb ${repo_name}
-
-  docker images
+      ./build_image.rb $*
 }
 
-build_image ${1}
-exit_status=$?
-echo "exit_status=${exit_status}"
+build_image $*
