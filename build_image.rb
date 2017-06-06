e2877a968f993c761f39232088a77356b4228391
Jon Jagger <jon@jaggersoft.com>
2017-06-06 10:26:58 +0100

    start code to process outputs/ and traffic_lights/ dirs

--- build_image.rb
+++ build_image.rb
@@ -3,12 +3,19 @@
 require_relative 'dependencies'
 require 'json'
 
+def success; 0; end
+def fail; 1; end
+
 def repo_url       ; ENV['REPO_URL'       ]; end
 def docker_username; ENV['DOCKER_USERNAME']; end
 def docker_password; ENV['DOCKER_PASSWORD']; end
 
-def      docker_dir; '/docker'     ; end
-def start_point_dir; '/start_point'; end
+def         docker_dir; '/docker'     ; end
+def    start_point_dir; '/start_point'; end
+def        outputs_dir; '/outputs'; end
+def traffic_lights_dir; '/traffic_lights'; end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def from
   dockerfile = IO.read("#{docker_dir}/Dockerfile")
@@ -17,21 +24,29 @@ def from
   from_line.split[1].strip
 end
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 def language_repo?
   File.exists?("#{docker_dir}/manifest.json") &&
     !Dir.exists?(start_point_dir)
 end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def test_framework_repo?
   !File.exists?("#{docker_dir}/manifest.json") &&
     Dir.exists?(start_point_dir)
 end
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 def json_image_name(filename)
   manifest = IO.read(filename)
   json = JSON.parse(manifest)
   json['image_name']
 end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def image_name
   if language_repo?
@@ -43,6 +58,8 @@ def image_name
   return nil
 end
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 def print(lines, stream)
   lines.each { |line| stream.puts line }
 end
@@ -50,6 +67,8 @@ end
 def print_diagnostic(lines)
   print(lines, STDERR)
 end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def banner_line
   '=' * 42
@@ -63,38 +82,68 @@ def banner_end
   print([ 'OK', banner_line, '', '' ], STDOUT)
 end
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def assert_system(command)
+  output = `#{command}`
+  status = $?.exitstatus
+  unless status == success
+    print_diagnostic [
+      "FAILED:command:#{command}",
+      "exit_status == #{status}"
+    ]
+    exit fail
+  end
+  output
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 def verify_dependency_settings
   banner __method__.to_s
-  status = dependencies.include?([ repo_url, from, image_name ])
-  unless status
+  found = dependencies.include?([ repo_url, from, image_name ])
+  unless found
     print_diagnostic [
       'ERROR: cannot find dependency entry for',
       "  repo_url:#{repo_url}",
       "  from:#{from}",
       "  image_name:#{image_name}"
     ]
-    exit status
+    exit fail
   end
   banner_end
 end
 
-def assert_system(command)
-  system(command)
-  status = $?.exitstatus
-  unless status == 0
-    print_diagnostic [
-      "FAILED:command:#{command}",
-      "exit_status == #{status}"
-    ]
-    exit status
-  end
-end
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def build_the_image
   banner __method__.to_s
   assert_system "cd #{docker_dir} && docker build --tag #{image_name} ."
   banner_end
 end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def check_images_red_amber_green_lambda_file
+  banner __method__.to_s
+  # check image has /usr/local/bin/red_amber_green.rb
+  # check it can be eval's into ruby
+  # check its a well-formed lambda
+  #
+  rag_filename = '/usr/local/bin/red_amber_green.rb'
+  output = assert_system "docker run --rm -it #{image_name} cat #{rag_filename}"
+  fn = eval(output)
+  rag = fn.call(stdout='ssd', stderr='sdsd', status=42)
+  unless [:red,:amber,:green].include? rag
+    print_diagnostic([
+      "FAILED:image #{image_name}'s #{rag_filename} not in [:red,:amber,:green]"
+    ])
+    exit fail
+  end
+  banner_end
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def check_start_point
   banner __method__.to_s
@@ -105,8 +154,58 @@ def check_start_point
   name = 'checking'
   assert_system "./#{script} start-point create #{name} --git=#{repo_url}"
   assert_system "./#{script} start-point rm #{name}"
+
+  # TODO: check start_point is red
+
   banner_end
 end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def check_outputs_colour(rag)
+  dir = "#{outputs_dir}/#{rag}"
+  if !Dir.exists? dir
+    print_diagnostic([ "WARNING: no #{dir}/ dir" ])
+  else
+    #TODO:
+  end
+end
+
+def check_outputs
+  banner __method__.to_s
+  if !Dir.exists? outputs_dir
+    print_diagnostic([ "WARNING: no #{outputs}/ dir" ])
+    return
+  end
+  check_outputs_colour('red')
+  check_outputs_colour('amber')
+  check_outputs_colour('green')
+  banner_end
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def check_traffic_lights_colour(rag)
+  dir = "#{traffic_lights_dir}/#{rag}"
+  if !Dir.exists? dir
+    print_diagnostic([ "WARNING: no #{dir}/ dir" ])
+  else
+    # TODO:
+  end
+end
+
+def check_traffic_lights
+  banner __method__.to_s
+  if !Dir.exists? traffic_lights_dir
+    print_diagnostic([ "WARNING: no #{traffic_lights}/ dir" ])
+    return
+  end
+  check_traffic_lights_colour('amber')
+  check_traffic_lights_colour('green')
+  banner_end
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def push_the_image
   banner __method__.to_s
@@ -115,19 +214,48 @@ def push_the_image
     "--username #{docker_username}",
     "--password #{docker_password}"
   ].join(' ')
-  assert_system command
+
+  # careful not to show password if command fails
+  `#{command}`
+  status = $?.exitstatus
+  unless status == success
+    secure = 'secure'
+    redacted = [
+      'docker login',
+      "--username [#{secure}]",
+      "--password [#{secure}]"
+    ].join(' ')
+    print_diagnostic([
+      "FAILED:command:#{redacted}",
+      "exit_status == #{status}"
+    ])
+    exit fail
+  end
+
   assert_system "docker push #{image_name}"
   assert_system 'docker logout'
   banner_end
 end
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def trigger_dependent_repos
+  banner __method__.to_s
+  # TODO:
+  banner_end
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 verify_dependency_settings
 build_the_image
 if test_framework_repo?
+  check_images_red_amber_green_lambda_file
   check_start_point
-  #check_traffic_lights/ (using runner)
-  #check_outputs/
+  check_outputs
+  check_traffic_lights
 end
-push_the_image
 
+push_the_image
+trigger_dependent_repos
 
