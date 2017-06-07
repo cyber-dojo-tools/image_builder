c602d416214d3b7d55a4993eeb9a0b0881080149
Jon Jagger <jon@jaggersoft.com>
2017-06-07 14:46:47 +0100

    added comments on dependency triple name design

--- dependencies.rb
+++ dependencies.rb
@@ -1,10 +1,48 @@
 #!/usr/bin/env ruby
 
-# each triple is
-#   [ name of repo which builds a language+TF docker image,
-#     name of docker image it is built FROM,
-#     name of docker image it builds
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Each triple is
+#   [ 1.name of repo which builds a docker image,
+#     2.name of docker image it is built FROM,
+#     3.name of docker image it builds
 #   ]
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 1. repo-name
+#    I'd like the repo-name to be named, eg,
+#    "#{cdl}/alpine-language-base:3.4
+#    but github does not allow a colon in the repo name
+#    so I'm using
+#    "#{cdl}/alpine-language-base-3.4
+#
+# 2. FROM-name
+#    This is exactly as it appears in the Dockerfile
+#    This does use a :version-number
+#
+# 3. image-name
+#    This also does use a :version-number
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# There are two kinds of triples:
+#
+# base-entries
+# - - - - - - -
+# These are for images which are, or helps to create,
+# base language repos which do not include a test
+# framework. These DO have version numbers.
+# Examples
+#   cyberdojofoundation/elm:0.18.0
+#   cyberdojofoundation/haskell:7.6.3
+#
+# language-entries
+# - - - - - - - - -
+# These are for images which do include a test
+# framwork. These do NOT have version numbers.
+# Examples
+#   cyberdojofoundation/elm_test
+#   cyberdojofoundation/haskell_hunit
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 def dependencies
   cdl = 'https://github.com/cyber-dojo-languages'
