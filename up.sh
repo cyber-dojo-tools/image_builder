58f29564c0da8e0ac33bb76b4f3dd93736ad2026
Jon Jagger <jon@jaggersoft.com>
2017-06-11 11:11:55 +0100

    added simple check to ensure services are up

--- up.sh
+++ up.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 set -e
 
+check_up()
+{
+  set +e
+  local s=$(docker ps --filter status=running --format '{{.Names}}' | grep ^${1}$)
+  set -e
+  if [ "${s}" != "${1}" ]; then
+    echo
+    echo "${1} exited"
+    docker logs ${1}
+    exit 1
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-
 docker-compose --file ${MY_DIR}/docker-compose.yml up -d
-
-# crude wait for Thin server in runner_stateless
-sleep 2
+# crude wait for services
+sleep 1
+check_up 'cyber-dojo-runner-stateless'
+check_up 'cyber-dojo-image-builder'
