96f943c11954471688225de3411d8c4cc0330e5e
Jon Jagger <jon@jaggersoft.com>
2017-06-11 18:55:13 +0100

    switched to using docker-compose.yml in run_build_image.sh file

--- run_build_image.sh
+++ run_build_image.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
+set -e
 
-WORK_DIR=${1:-`pwd`}
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+
+export WORK_DIR=${1:-`pwd`}
 
 if [ ! -d "${WORK_DIR}" ]; then
   echo "FAILED: ${WORK_DIR} dir does not exist"
   exit 1
 fi
 
-docker run \
-    --rm \
-    --interactive \
-    --tty \
-    --env DOCKER_USERNAME=${DOCKER_USERNAME} \
-    --env DOCKER_PASSWORD=${DOCKER_PASSWORD} \
-    --env GITHUB_TOKEN=${GITHUB_TOKEN} \
-    --env WORK_DIR=${WORK_DIR} \
-    --volume=/var/run/docker.sock:/var/run/docker.sock \
-    --volume=${WORK_DIR}:/repo:ro \
-    cyberdojofoundation/image_builder \
-      ./build_image.rb
+${MY_DIR}/build.sh
+${MY_DIR}/up.sh
+docker exec -it cyber-dojo-image-builder /app/build_image.rb
