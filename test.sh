9a077845d1f3c7aecf1b2e25721d1a265734fb4e
Jon Jagger <jon@jaggersoft.com>
2017-06-13 10:10:09 +0100

    refactoring; comment update

--- test.sh
+++ test.sh
@@ -4,8 +4,13 @@ set -e
 docker exec cyber-dojo-image-builder /app/spike-curl-run.sh
 
 # TODO: shunit2
+#
 # use test/good_language
-#   change a file and verify failure is as expected
+#   copy it to /tmp
+#   change something specific in /tmp
+#   verify the failure is as expected
 
 # use test/good_test
-#   change a file and verify failure is as expected
+#   copy it to /tmp
+#   change something specific in /tmp
+#   verify the failure is as expected
