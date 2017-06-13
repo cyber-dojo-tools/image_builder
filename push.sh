c0984712f4e46a6780567ce52af18bc0c128e467
Jon Jagger <jon@jaggersoft.com>
2017-06-13 12:33:29 +0100

    pipe-script has to push two images now image-builder is split and has outer and inner images

--- push.sh
+++ push.sh
@@ -6,4 +6,6 @@ readonly ORG_NAME=cyberdojofoundation
 readonly TAG_NAME=$(basename ${MY_DIR})
 
 docker login --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD}
-docker push ${ORG_NAME}/${TAG_NAME}
+
+docker push ${ORG_NAME}/image_builder_inner
+docker push ${ORG_NAME}/image_builder
