e2f508b10bb98a8c93cb1c562c4d2ad7c5fdaa4a
Jon Jagger <jon@jaggersoft.com>
2017-06-13 08:02:04 +0100

    refactoring

--- build_image.sh
+++ build_image.sh
@@ -20,12 +20,14 @@ echo "DOCKER_PASSWORD=:${DOCKER_PASSWORD}:"
 echo "WORK_DIR=:${WORK_DIR}:"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly NAME=language
 
-docker volume create --name=language
+docker volume create --name=${NAME}
+
 readonly cid=$(docker create \
   --interactive \
   --user=root \
-  --volume=language:/repo \
+  --volume=${NAME}:/repo \
     cyberdojo/runner \
       sh)
 
@@ -33,6 +35,10 @@ docker cp ${WORK_DIR}/. ${cid}:/repo
 
 #docker stop ${cid}
 #docker rm --volumes ${cid}
+
+#TODO: docker-compose            up runner
+#TODO: docker-compose            up runner-stateless
+#TODO: docker-compose -e KEY=VAL run builder
 
 docker-compose --file ${MY_DIR}/docker-compose2.yml up -d
 
@@ -42,7 +48,7 @@ check_up 'cyber-dojo-runner'
 check_up 'cyber-dojo-runner-stateless'
 
 #TODO: GITHUB_TOKEN
-#NOT GOING TO WORK. There is no --volume option on [docker-compose run]
+
 docker exec \
   --interactive \
   --tty \
