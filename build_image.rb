372a4f0a46c99e79ae32ad9a08b8d19d70f710a2
Jon Jagger <jon@jaggersoft.com>
2017-06-13 17:04:02 +0100

    reduce amount of output run_build_image.sh produces

--- build_image.rb
+++ build_image.rb
@@ -18,9 +18,9 @@ def my_dir; '/app'; end
 def shell(command)
   output = `#{command}`
   status = $?.exitstatus
-  log( [ "command=:#{command}:"] )
-  log( [ "status=:#{status}:" ])
-  log( [ "output=:#{output}:" ])
+  #log( [ "command=:#{command}:"] )
+  #log( [ "status=:#{status}:" ])
+  #log( [ "output=:#{output}:" ])
   return output.strip, status
 end
 
@@ -30,6 +30,14 @@ def assert_shell(command)
     failed [ command, "exit_status == #{status}", output ]
   end
   output.strip
+end
+
+def assert_system(command)
+  system(command)
+  status = $?.exitstatus
+  unless status == success
+    failed [ command, "exit_status == #{status}", output ]
+  end
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -145,7 +153,9 @@ docker_compose 'up -d runner_stateless'
 wait_till_up 'cyber-dojo-runner'
 wait_till_up 'cyber-dojo-runner-stateless'
 
-docker_compose [
+assert_system [
+  'docker-compose',
+    "--file #{my_dir}/docker-compose.yml",
     'run',
       "-e DOCKER_USERNAME=#{docker_username}",
       "-e DOCKER_PASSWORD=#{docker_password}",
