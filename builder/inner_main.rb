2a1845ef071769d87681f4d755fbe9329dc06229
Jon Jagger <jon@jaggersoft.com>
2017-08-22 09:21:22 +0530

    [ci skip] added comments

--- builder/inner_main.rb
+++ builder/inner_main.rb
@@ -40,7 +40,10 @@ puts
 # TODO:
 # Running Travis
 # Send POST to trigger immediate dependents.
+# Probably will involve installing npm and then
+# curling the trigger.js file used in cyber-dojo repos.
+#
 #
 # Running locally
-# graph-chain-build all dependents
+# graph-chain-build all dependents?
 
