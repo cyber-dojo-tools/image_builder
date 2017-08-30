659ecb2a31cf267ba1904bcb2bb66b82fba197c7
Jon Jagger <jon@jaggersoft.com>
2017-08-30 06:32:33 +0000

    refactoring

--- run_build_image.sh
+++ run_build_image.sh
@@ -30,23 +30,21 @@ shift # ${1}
 
 if [ ! -z "${TRAVIS}" ]; then
   echo 'Running on TRAVIS'
-  readonly BASE_DIR=${SRC_DIR}
 else
   echo 'Running locally'
-  readonly BASE_DIR=$(dirname ${SRC_DIR})
 fi
 
-# I create a data-volume-container which holds src-dir/..
+# I create a data-volume-container which holds src-dir
 # By default this lives on one network and the containers
 # created inside image_builder (from its docker-compose.yml file)
 # live on a different network, and thus the later won't be able
-# to see to the former. To solve this I'm putting the src-dir/..
+# to see to the former. To solve this I'm putting the src-dir
 # data-volume-container into its own dedicated network.
 
 docker network create ${NETWORK} > /dev/null
 
 docker create \
-  --volume=${BASE_DIR}:${BASE_DIR} \
+  --volume=${SRC_DIR}:${SRC_DIR} \
   --name=${NAME} \
   --network=${NETWORK} \
   cyberdojofoundation/image_builder \
@@ -66,7 +64,7 @@ docker run \
   --env TRAVIS_REPO_SLUG \
   --volume=/var/run/docker.sock:/var/run/docker.sock \
     cyberdojofoundation/image_builder \
-      /app/outer_main.rb ${*}
+      /app/outer_main.rb
 
 exit_status=$?
 
