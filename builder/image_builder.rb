45299cc916434a2cea1454d88a284ef5d2984225
Jon Jagger <jon@jaggersoft.com>
2017-09-05 15:11:03 +0100

    install bash in Dockerfile to run curl'd cyber-dojo script

--- builder/image_builder.rb
+++ builder/image_builder.rb
@@ -15,8 +15,7 @@ class ImageBuilder
 
   def build_and_test_image
     if test_framework?
-      #TODO: check why this is failing
-      #check_start_point_can_be_created
+      check_start_point_can_be_created
     end
     build_the_image
     if test_framework?
