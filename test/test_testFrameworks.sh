625487781e7abc088c8ba389e1351fa57f9b085e
Jon Jagger <jon@jaggersoft.com>
2017-10-27 15:00:52 +0100

    remove cat/echo from test-scripts

--- test/test_testFrameworks.sh
+++ test/test_testFrameworks.sh
@@ -16,8 +16,6 @@ test___success() { :; }
 test_____alpine_testFramework_stateful()
 {
   build_image /test/test-frameworks/alpine-gcc-assert/stateful
-  cat ${stdoutF}
-  echo ${TRAVIS_REPO_SLUG}
   assertStdoutIncludes '# check_start_point_can_be_created'
   assertStdoutIncludes "adduser -D -G cyber-dojo -h /home/flamingo -s '/bin/sh' -u 40014 flamingo"
   assertStdoutIncludes 'Welcome to Alpine Linux 3.6'
@@ -31,8 +29,6 @@ test_____alpine_testFramework_stateful()
 test_____ubuntu_testFramework_stateless()
 {
   build_image /test/test-frameworks/ubuntu-python-pytest/stateless
-  cat ${stdoutF}
-  echo ${TRAVIS_REPO_SLUG}
   assertStdoutIncludes '# check_start_point_can_be_created'
   assertStdoutIncludes "adduser --disabled-password --gecos \"\" --ingroup cyber-dojo --home /home/flamingo --uid 40014 flamingo"
   assertStdoutIncludes 'Ubuntu 17.04'
