ec22c84680381d876b93bcbf8b22f202a9148ef1
Jon Jagger <jon@jaggersoft.com>
2017-10-29 07:16:28 +0000

    refactoring

--- builder/dir_get_args.rb
+++ builder/dir_get_args.rb
@@ -5,29 +5,10 @@ module DirGetArgs
   include JsonParse
 
   def dir_get_args(src_dir)
-    args = []
-    args << (image_name_filename = src_dir + '/docker/image_name.json')
-    args << (manifest_filename   = src_dir + '/start_point/manifest.json')
-    args << (image_name_file = read_nil(image_name_filename))
-    args << (manifest_file   = read_nil(manifest_filename))
-    {
-      image_name:get_image_name(args)
-    }
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def read_nil(filename)
-    File.exists?(filename) ? IO.read(filename) : nil
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def get_image_name(args)
-    image_name_filename = args[0]
-    manifest_filename   = args[1]
-    image_name_content  = args[2]
-    manifest_content    = args[3]
+    image_name_filename = src_dir + '/docker/image_name.json'
+    manifest_filename   = src_dir + '/start_point/manifest.json'
+    image_name_content = read_nil(image_name_filename)
+    manifest_content   = read_nil(manifest_filename)
 
     either_or = [
       "#{image_name_filename} must exist",
@@ -52,7 +33,18 @@ module DirGetArgs
       filename = manifest_filename
       content = manifest_content
     end
-    json_parse(filename, content)['image_name']
+
+    image_name = json_parse(filename, content)['image_name']
+
+    {
+      image_name:image_name
+    }
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def read_nil(filename)
+    File.exists?(filename) ? IO.read(filename) : nil
   end
 
 end
