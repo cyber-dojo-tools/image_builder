1a8a25c79a32780f2407e442ad4a14c2101be69b
Jon Jagger <jon@jaggersoft.com>
2017-10-29 05:34:45 +0000

    refactoring

--- builder/inner_main.rb
+++ builder/inner_main.rb
@@ -3,34 +3,24 @@
 require_relative 'image_builder'
 require_relative 'travis'
 
-class InnerMain
-
-  def run
-    builder = ImageBuilder.new
-    builder.build_image
-    builder.create_start_point
-    builder.test_red_amber_green
-
-    if on_travis_cyber_dojo?
-      travis = Travis.new
-      travis.validate_image_data_triple
-      travis.push_image_to_dockerhub
-      travis.trigger_dependents
-    end
-  end
-
-  private
-
-  def on_travis_cyber_dojo?
-    # return false if we are running our own tests
-    repo_slug = ENV['TRAVIS_REPO_SLUG']
-    ENV['TRAVIS'] == 'true' &&
-      repo_slug != 'cyber-dojo-languages/image_builder' &&
-      (repo_slug.start_with?('cyber-dojo-languages/') ||
-       repo_slug.start_with?('cyber-dojo/'))
-
-  end
+def on_travis_cyber_dojo?
+  # return false if we are running our own tests
+  repo_slug = ENV['TRAVIS_REPO_SLUG']
+  ENV['TRAVIS'] == 'true' &&
+    repo_slug != 'cyber-dojo-languages/image_builder' &&
+    (repo_slug.start_with?('cyber-dojo-languages/') ||
+     repo_slug.start_with?('cyber-dojo/'))
 
 end
 
-InnerMain.new.run
+builder = ImageBuilder.new
+builder.build_image
+builder.create_start_point
+builder.test_red_amber_green
+
+if on_travis_cyber_dojo?
+  travis = Travis.new
+  travis.validate_image_data_triple
+  travis.push_image_to_dockerhub
+  travis.trigger_dependents
+end
