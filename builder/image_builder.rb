fa971e6cd6fd740868714dc4eaf8994b3d309992
Jon Jagger <jon@jaggersoft.com>
2017-10-29 19:06:04 +0000

    fix broken edit

--- builder/image_builder.rb
+++ builder/image_builder.rb
@@ -49,7 +49,7 @@ class ImageBuilder
       assert_system "chmod +x #{script}"
       name = 'start-point-create-check'
       system "./#{script} start-point rm #{name} &> /dev/null"
-      assert_system "./#{script} start-point create #{name} --dir=#{source.start_point.dir}"
+      assert_system "./#{script} start-point create #{name} --dir=#{source.dir}"
       print_to STDOUT, 'start point can be created'
     }
   end
