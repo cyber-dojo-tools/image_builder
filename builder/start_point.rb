c24b371f2302c1a54dc6f13158cfeeb7172e9fe1
Jon Jagger <jon@jaggersoft.com>
2017-10-31 10:15:18 +0000

    remove start-point volume after checking it can be created

--- builder/start_point.rb
+++ builder/start_point.rb
@@ -23,8 +23,11 @@ class StartPoint
       assert_system "curl --silent -O #{url}"
       assert_system "chmod +x #{script}"
       name = 'start-point-create-check'
-      system "./#{script} start-point rm #{name} &> /dev/null"
-      assert_system "./#{script} start-point create #{name} --dir=#{src_dir}"
+      rm_cmd = "./#{script} start-point rm #{name} &> /dev/null"
+      create_cmd = "./#{script} start-point create #{name} --dir=#{src_dir}"
+      system rm_cmd
+      assert_system create_cmd
+      assert_system rm_cmd
       print_to STDOUT, 'start point can be created'
     }
   end
