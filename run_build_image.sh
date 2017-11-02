9f17bb81701d0431a86735e618eedcc697d093b0
Jon Jagger <jon@jaggersoft.com>
2017-11-02 15:39:50 +0000

    fix argument checking and improve diagnostic output

--- run_build_image.sh
+++ run_build_image.sh
@@ -1,38 +1,62 @@
 #!/bin/bash
 set -e
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Runs image-builder on source living in SRC_DIR to
 #   o) build zero or more docker-images
 #   o) test zero or more start-point directories.
 #
 # This script is curl'd and run in the .travis.yml script
 # of each cyber-dojo-language (github org) repo.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 readonly SRC_DIR=${1:-${PWD}}
 readonly NETWORK=src_dir_network
 readonly NAME=src_dir_container
 
-# TODO?: check if $SRC_DIR is relative and if it is
-#       expand it based on PWD, otherwise you cant create the volume
-
 check_use()
 {
-  if [ -z "${SRC_DIR}" ] || [ "${1}" == '--help' ]; then
-    show_use
+  if [ "${1}" == '--help' ]; then
+    show_use_long
     exit 1
   fi
   if [ ! -d "${SRC_DIR}" ]; then
-    show_use
-    echo "SRC_DIR <${SRC_DIR}> does not exist"
+    show_use_short
+    echo "error: SRC_DIR <${SRC_DIR}> does not exist"
     exit 1
   fi
 }
 
-show_use() {
-  echo 'Use: run_build_image.sh <SRC_DIR> '
+show_use_short()
+{
+  echo "Use: $(basename $0) [SRC_DIR|--help]"
   echo ''
-  echo '  <SRC_DIR> defaults to ${PWD}'
+  echo '  SRC_DIR defaults to ${PWD}'
+  echo '  SRC_DIR must be an absolute path'
   echo ''
+  # TODO?: SRC_DIR must be absolute because you cant create
+  #        a docker-volume from a relative path.
+  #        Check if $SRC_DIR is relative and if it is
+  #       expand it based on PWD ?
+}
+
+show_use_long()
+{
+  show_use_short
+  echo 'If SRC_DIR/start_point_type.json exists this script will'
+  echo 'verify that a cyber-dojo start-point can be created from SRC_DIR, viz'
+  echo '  $ cyber-dojo start-point create ... --dir=${SRC_DIR}'
+  echo ''
+  echo 'If SRC_DIR (or any sub-dir) contains a Dockerfile this script will'
+  echo 'verify, for each one, that a docker-image can be created from it.'
+  echo ''
+  echo 'If SRC_DIR (or any sub-dir) contains a manifest.json this script will'
+  echo 'verify, for each one, that a start-point entry can be created from it.'
+  echo ''
+  echo 'In each Dockerfile dir there must be an image_name.json specifying'
+  echo 'the name of docker image to create from the Dockerfile. However,'
+  echo 'if there is a single Dockerfile and a single manifest.json then the'
+  echo 'name of the docker image will be taken from the manifest.json file.'
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - -
@@ -121,7 +145,7 @@ exit_handler()
   network_remove
 }
 
-check_use
+check_use $*
 trap exit_handler INT EXIT
 show_location
 volume_create
