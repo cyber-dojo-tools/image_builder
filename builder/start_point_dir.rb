cae14e0bb081e1697972c23b8142be809c6eefe1
Jon Jagger <jon@jaggersoft.com>
2017-11-06 08:27:57 +0000

    add todo comment

--- builder/start_point_dir.rb
+++ builder/start_point_dir.rb
@@ -22,6 +22,8 @@ class StartPointDir
   def test_run
     # TODO: check the image_name has the
     # 64 avatars users inside it.
+    # TODO: check the image_name has the
+    # #{rag_filename} inside it.
     hhg = options? || filename_6_times_9?
     # TODO: If being run on a cyber-dojo-langauges
     # repo then check it _HAS_ got 6*9 content
