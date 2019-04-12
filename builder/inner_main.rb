09811a92625d96b9af6fea999b303e920a0da268
Jon Jagger <jon@jaggersoft.com>
2019-04-12 21:26:04 +0100

    comment out creating start-point ready for switch to start-points being images

--- builder/inner_main.rb
+++ builder/inner_main.rb
@@ -4,6 +4,6 @@ require_relative 'src_dir'
 
 src_dir = SourceDir.new(ENV['SRC_DIR'])
 if src_dir.start_point?
-  src_dir.assert_create_start_point
+  #src_dir.assert_create_start_point
 end
 src_dir.check_all
