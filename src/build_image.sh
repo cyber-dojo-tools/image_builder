854aa17e2ef1da8c090314831167bdc83d273aca
Jon Jagger <jon@jaggersoft.com>
2019-05-29 12:52:11 +0100

    add simple checking of  to src/build_image.sh

--- src/build_image.sh
+++ src/build_image.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-# TODO: check $1 == "" is false
-# TODO: check [ -d $1 ] is true
-
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-# TODO: build images here for now.
-"${MY_DIR}/dockerfile_augmenter/build_image.sh"
-"${MY_DIR}/image_namer/build_image.sh"
-"${MY_DIR}/dependents_notifier/build_image.sh"
+build_tools() # here for now
+{
+  "${MY_DIR}/dockerfile_augmenter/build_image.sh"
+  "${MY_DIR}/image_namer/build_image.sh"
+  "${MY_DIR}/dependents_notifier/build_image.sh"
+}
 
 # - - - - - - - - - - - - - - - - - - - -
 
@@ -22,16 +21,20 @@ remove_context_dir()
 
 trap remove_context_dir EXIT
 
+# - - - - - - - - - - - - - - - - - - - -
+
 absPath()
 {
   cd "$(dirname "$1")"
   printf "%s/%s\n" "$(pwd)" "$(basename "$1")"
 }
 
-readonly START_POINT_DIR=`absPath "${1}"`
+# - - - - - - - - - - - - - - - - - -
 
 build_image()
 {
+  local START_POINT_DIR=`absPath "${1}"`
+
   # Find out the name of the docker-image.
   local IMAGE_NAME=$(docker run \
     --rm \
@@ -45,7 +48,6 @@ build_image()
 
   # Overwrite the Dockerfile with one containing
   # extra commands to fulfil the runner's requirements.
-
   cat "${START_POINT_DIR}/docker/Dockerfile" \
     | \
       docker run --rm \
@@ -56,16 +58,28 @@ build_image()
       "${CONTEXT_DIR}/Dockerfile"
 
   # Write new Dockerfile to stdout in case of debugging
-
   echo '# ~~~~~~~~~~~~~~~~~~~~~~~~~'
   cat "${CONTEXT_DIR}/Dockerfile"
   echo '# ~~~~~~~~~~~~~~~~~~~~~~~~~'
 
-  # Build the docker-image!
-
+  # Build the docker-image
   docker build \
     --tag "${IMAGE_NAME}" \
     "${CONTEXT_DIR}"
 }
 
-build_image
+# - - - - - - - - - - - - - - - - - -
+
+if [ "${1}" = '' ]; then
+  echo 'Use: build_image.sh DIR'
+  exit 1
+fi
+
+if [ ! -d "${1}" ]; then
+  echo 'Use: build_image.sh DIR'
+  echo 'DIR does not exist'
+  exit 1
+fi
+
+build_tools
+build_image "${1}"
