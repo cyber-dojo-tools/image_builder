39c8be82e8afa2b847be0bad84e6e5c5c0fd0eb2
Jon Jagger <jon@jaggersoft.com>
2019-05-29 10:07:39 +0100

    delete dead files

--- tools/build_augment_docker_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-
-docker build \
-  --file "${MY_DIR}/Dockerfile.augment" \
-  --tag cyberdojo/augment_dockerfile \
-  "${MY_DIR}" > /dev/null
