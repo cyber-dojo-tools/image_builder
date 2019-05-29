ebf3b1be3edb36e2be8b802a247a2221044d95c2
Jon Jagger <jon@jaggersoft.com>
2019-05-29 06:21:30 +0100

    added todo notes

--- src/build_image.sh
+++ src/build_image.sh
@@ -3,13 +3,26 @@
 # TODO: check $1 == "" is false
 # TODO: check [ -d $1 ] is true
 
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+
+# TODO: build images here for now.
+# Will need to be pushed to dockerhub in CI pipe.
+# Put them into their own repos?
+"${MY_DIR}/dockerfile_augmenter/build_image.sh"
+"${MY_DIR}/image_namer/build_image.sh"
+
+# - - - - - - - - - - - - - - - - - - - -
+
 absPath()
 {
   cd "$(dirname "$1")"
   printf "%s/%s\n" "$(pwd)" "$(basename "$1")"
 }
 
-readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+# TODO: 2 helper scripts will need to folded into
+# the main run_build_image.sh script which is curl'd
+# by each cyber-dojo-languages repo's CI script.
+
 readonly START_POINT_DIR=`absPath "${1}"`
 readonly IMAGE_NAME=$("${MY_DIR}/name_image.sh" "${START_POINT_DIR}")
 
