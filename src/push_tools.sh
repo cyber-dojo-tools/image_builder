c450b80c7b64d0d80eed40acb169d79ae979aa58
Jon Jagger <jon@jaggersoft.com>
2019-05-30 10:13:16 +0100

    tidy scripts

--- src/push_tools.sh
+++ src/push_tools.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
+set -e
 
-push_tools()
-{
-  docker push cyberdojo/dockerfile_augmenter
-  docker push cyberdojo/image_namer
-  docker push cyberdojo/dependents_notifier
-}
-
-push_tools
+docker push cyberdojo/dockerfile_augmenter
+docker push cyberdojo/image_namer
+docker push cyberdojo/dependents_notifier
