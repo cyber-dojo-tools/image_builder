350c5bffe859b0df44c8cff6e250e46db7b13493
Jon Jagger <jon@jaggersoft.com>
2019-06-15 14:23:00 +0100

    refactoring; add dir/ structure to dockerfile_augmenter

--- src/build_tools.sh
+++ src/build_tools.sh
@@ -3,6 +3,6 @@ set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-"${MY_DIR}/dockerfile_augmenter/build_image.sh"
+"${MY_DIR}/dockerfile_augmenter/sh/build_image.sh"
 "${MY_DIR}/image_namer/build_image.sh"
 "${MY_DIR}/dependents_notifier/build_image.sh"
--- src/dockerfile_augmenter/sh/build_image.sh
+++ src/dockerfile_augmenter/sh/build_image.sh
@@ -4,4 +4,4 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 docker build \
   --tag cyberdojofoundation/dockerfile_augmenter \
-  "${MY_DIR}"
+  "${MY_DIR}/.."
