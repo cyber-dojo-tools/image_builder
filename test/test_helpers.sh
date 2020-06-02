e07e8085865c657c90ff9997f88cf6889eed94a6
Jon Jagger <jon@jaggersoft.com>
2020-06-02 11:53:00 +0100

    Try adding -t to docker run command

--- test/test_helpers.sh
+++ test/test_helpers.sh
@@ -54,10 +54,10 @@ assert_image_OS()
   set -x
   local image_name="${1}"
   local os="${2}"
-  local etc_issue="$(docker run --rm -i ${image_name} bash -c 'cat /etc/issue | head -n +1')"
+  local etc_issue="$(docker run --rm -it ${image_name} bash -c 'cat /etc/issue | head -n +1')"
   local diagnostic="${image_name} is NOT based on ${os}..."
   echo 9
-  echo "${etc_issue}" | grep --silent "${os}" 
+  echo "${etc_issue}" | grep --silent "${os}"
   assertTrue "${diagnostic}" $?
   echo 10
   echo -e "\t- image-name is ${image_name}"
