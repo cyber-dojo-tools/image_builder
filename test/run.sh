947088d3ef8cb72be18be622b1fc3a6e0f001688
Jon Jagger <jon@jaggersoft.com>
2020-06-04 14:15:12 +0100

    If docker is not installed, install it

--- test/run.sh
+++ test/run.sh
@@ -2,6 +2,9 @@
 
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ ! -x docker ]; then
+  curl -sSL https://get.docker.com/ | sh
+fi
 # in order dependency...
 ${my_dir}/test_languages.sh
 ${my_dir}/test_testFrameworks.sh
