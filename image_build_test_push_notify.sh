e9d785cf89100451dd392ca7aa7e35269b68f01a
Jon Jagger <jon@jaggersoft.com>
2020-06-08 19:00:56 +0100

    Turn off start-point image creation till dockerhub org setup

--- image_build_test_push_notify.sh
+++ image_build_test_push_notify.sh
@@ -540,7 +540,7 @@ else
 fi
 
 if on_CI && ! scheduled_CI && ! testing_myself; then
-  build_start_point_image_and_push_to_dockerhub
+  #build_start_point_image_and_push_to_dockerhub
   push_cdl_images_to_dockerhub
   # notify_dependent_projects # Off
 else
