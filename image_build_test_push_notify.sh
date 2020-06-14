ce85860f407c015376eae224ebaf636ee115507d
Jon Jagger <jon@jaggersoft.com>
2020-06-14 09:52:13 +0100

    Fix main script so [start-point build] command is given tagged-url

--- image_build_test_push_notify.sh
+++ image_build_test_push_notify.sh
@@ -10,8 +10,6 @@
 # (as a default volume-mount) inside the VM being used.
 # - - - - - - - - - - - - - - - - - - - - - - -
 
-readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-readonly SRC_DIR=${1:-${PWD}}
 readonly TMP_DIR=$(mktemp -d ~/tmp-cyber-dojo.image_builder.XXXXXX)
 remove_tmp_dir() { rm -rf "${TMP_DIR}" > /dev/null; }
 
@@ -27,14 +25,14 @@ trap_handler()
 trap trap_handler EXIT
 
 # - - - - - - - - - - - - - - - - - - - - - - -
-readonly MY_NAME=$(basename $0)
 show_use_short()
 {
-  echo "Use: ${MY_NAME} [SRC_DIR|-h|--help]"
+  local -r my_name=$(basename ${BASH_SOURCE[0]})
+  echo "Use: ${my_name} [GIT_REPO_DIR|-h|--help]"
   echo ''
-  echo '  SRC_DIR defaults to ${PWD}.'
-  echo '  SRC_DIR/docker/Dockerfile.base must exist.'
-  echo '  SRC_DIR must contain a git repo.'
+  echo '  GIT_REPO_DIR defaults to ${PWD}.'
+  echo '  GIT_REPO_DIR must hold a git repo.'
+  echo '  GIT_REPO_DIR/docker/Dockerfile.base must exist.'
   echo ''
 }
 
@@ -44,27 +42,27 @@ show_use_long()
   show_use_short
   cat <<- EOF
   Step 1.
-  Creates \${SRC_DIR}/docker/Dockerfile from \${SRC_DIR}/docker/Dockerfile.base
+  Creates \${GIT_REPO_DIR}/docker/Dockerfile from \${GIT_REPO_DIR}/docker/Dockerfile.base
     augmented to fulfil the runner service's requirements.
-  Uses \${SRC_DIR}/docker/Dockerfile to build a docker image.
+  Uses \${GIT_REPO_DIR}/docker/Dockerfile to build a docker image.
   The name of the docker-image is:
-    the 'image_name' property of \${SRC_DIR}/start_point/manifest.json, if it exists,
-    otherwise of \${SRC_DIR}/docker/image_name.json.
+    the 'image_name' property of \${GIT_REPO_DIR}/start_point/manifest.json, if it exists,
+    otherwise of \${GIT_REPO_DIR}/docker/image_name.json.
   Embeds an env-var of the git commit sha inside this image:
-    SHA=\$(cd \${SRC_DIR} && git rev-parse HEAD)
+    SHA=\$(cd \${GIT_REPO_DIR} && git rev-parse HEAD)
 
   Step 2.
-  If \${SRC_DIR}/start_point/ exists:
-    *) Attempts to build a start-point image from the git-cloneable \${SRC_DIR}.
-       $ cyber-dojo start-point build ... --languages \${SRC_DIR}
+  If \${GIT_REPO_DIR}/start_point/ exists:
+    *) Attempts to build a start-point image from the git-cloneable \${GIT_REPO_DIR}.
+       $ cyber-dojo start-point build ... --languages \${GIT_REPO_DIR}
 
-    *) Verifies the red|amber|green traffic-lights for \${SRC_DIR}/start_point/ files
+    *) Verifies the red|amber|green traffic-lights for \${GIT_REPO_DIR}/start_point/ files
        run against the image created in Step 1.
          o) unmodified, give a red traffic-light.
          o) with '6 * 9' replaced by '6 * 9sd', give an amber traffic-light.
          o) with '6 * 9' replaced by '6 * 7', give a green traffic-light.
-       If there is no \${SRC_DIR}/start_point/ file containing '6 * 9',
-       looks for the file \${SRC_DIR}/start_point/options.json. For example, see:
+       If there is no \${GIT_REPO_DIR}/start_point/ file containing '6 * 9',
+       looks for the file \${GIT_REPO_DIR}/start_point/options.json. For example, see:
        https://github.com/cyber-dojo-languages/nasm-assert/tree/master/start_point
 
   Step 3.
@@ -86,21 +84,22 @@ exit_zero_if_show_help()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
-exit_non_zero_unless_good_SRC_DIR()
+exit_non_zero_unless_good_GIT_REPO_DIR()
 {
-  if [ ! -d "${SRC_DIR}" ]; then
+  local -r git_repo_dir="${1:-${PWD}}"
+  if [ ! -d "${git_repo_dir}" ]; then
     show_use_short
-    echo "error: ${SRC_DIR} does not exist"
+    stderr "ERROR: ${git_repo_dir} does not exist."
     exit 42
   fi
-  if [ ! -f "${SRC_DIR}/docker/Dockerfile.base" ]; then
+  if [ ! -f "${git_repo_dir}/docker/Dockerfile.base" ]; then
     show_use_short
-    echo "error: ${SRC_DIR}/docker/Dockerfile.base does not exist"
+    stderr "ERROR: ${git_repo_dir}/docker/Dockerfile.base does not exist."
     exit 42
   fi
-  if [ ! $(cd ${SRC_DIR} && git rev-parse HEAD 2> /dev/null) ]; then
+  if [ ! $(cd ${git_repo_dir} && git rev-parse HEAD 2> /dev/null) ]; then
     show_use_short
-    echo "error: ${SRC_DIR} is not in a git repo"
+    stderr "ERROR: ${git_repo_dir} is not in a git repo."
     exit 42
   fi
 }
@@ -124,47 +123,21 @@ exit_non_zero_unless_docker_installed()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
-ip_address()
+set_git_repo_dir()
 {
-  if [ -n "${DOCKER_MACHINE_NAME}" ]; then
-    docker-machine ip ${DOCKER_MACHINE_NAME}
-  else
-    echo localhost
-  fi
-}
-readonly IP_ADDRESS=$(ip_address)
-
-# - - - - - - - - - - - - - - - - - - - - - - -
-cyber_dojo()
-{
-  local -r name=cyber-dojo
-  if [ -x "$(command -v ${name})" ]; then
-    >&2 echo "Found executable ${name} on the PATH"
-    echo "${name}"
-  else
-    local -r url="https://raw.githubusercontent.com/cyber-dojo/commander/master/${name}"
-    >&2 echo "Did not find executable ${name} on the PATH"
-    >&2 echo "Curling it from ${url}"
-    curl --fail --output "${TMP_DIR}/${name}" --silent "${url}"
-    chmod 700 "${TMP_DIR}/${name}"
-    echo "${TMP_DIR}/${name}"
-  fi
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - -
-set_git_repo_url()
-{
-  echo "Checking $(src_dir_abs)"
+  local -r src_dir="${1:-${PWD}}"
+  local -r abs_src_dir="$(cd "${src_dir}" && pwd)"
+  echo "Checking ${abs_src_dir}"
   echo 'Looking for uncommitted changes'
-  if [[ -z $(cd $(src_dir_abs) && git status -s) ]]; then
+  if [ -z $(cd ${abs_src_dir} && git status -s) ]; then
     echo 'Found none'
-    echo "Using $(src_dir_abs)"
-    GIT_REPO_URL="$(src_dir_abs)"
+    echo "Using ${abs_src_dir}"
+    GIT_REPO_DIR="${abs_src_dir}"
   else
     echo 'Found some'
-    local -r url="${TMP_DIR}/$(basename $(src_dir_abs))"
+    local -r url="${TMP_DIR}/$(basename ${abs_src_dir})"
     echo "So copying it to ${url}"
-    cp -r "$(src_dir_abs)" "${TMP_DIR}"
+    cp -r "${abs_src_dir}" "${TMP_DIR}"
     echo "Committing the changes in ${url}"
     cd ${url}
     git config user.email 'cyber-dojo-machine-user@cyber-dojo.org'
@@ -172,14 +145,20 @@ set_git_repo_url()
     git add .
     git commit -m 'Save'
     echo "Using ${url}"
-    GIT_REPO_URL="${url}"
+    GIT_REPO_DIR="${url}"
   fi
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+stderr()
+{
+  >&2 echo "${1}"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 git_commit_sha()
 {
-  echo "$(cd "$(src_dir_abs)" && git rev-parse HEAD)"
+  echo "$(cd "${GIT_REPO_DIR}" && git rev-parse HEAD)"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
@@ -190,10 +169,27 @@ git_commit_tag()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
+cyber_dojo()
+{
+  local -r name=cyber-dojo
+  if [ -x "$(command -v ${name})" ]; then
+    stderr "Found executable ${name} on the PATH"
+    echo "${name}"
+  else
+    local -r url="https://raw.githubusercontent.com/cyber-dojo/commander/master/${name}"
+    stderr "Did not find executable ${name} on the PATH"
+    stderr "Curling it from ${url}"
+    curl --fail --output "${TMP_DIR}/${name}" --silent "${url}"
+    chmod 700 "${TMP_DIR}/${name}"
+    echo "${TMP_DIR}/${name}"
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - -
 build_cdl_image()
 {
-  echo "Creating file $(src_dir_abs)/docker/Dockerfile from $(src_dir_abs)/docker/Dockerfile.base"
-  cat "$(src_dir_abs)/docker/Dockerfile.base" \
+  echo "Creating file ${GIT_REPO_DIR}/docker/Dockerfile from ${GIT_REPO_DIR}/docker/Dockerfile.base"
+  cat "${GIT_REPO_DIR}/docker/Dockerfile.base" \
     | \
       docker run \
         --interactive \
@@ -201,22 +197,15 @@ build_cdl_image()
         --volume /var/run/docker.sock:/var/run/docker.sock \
         cyberdojofoundation/image_dockerfile_augmenter \
     > \
-      "$(src_dir_abs)/docker/Dockerfile"
+      "${GIT_REPO_DIR}/docker/Dockerfile"
 
-  echo "Building image $(image_name) from $(src_dir_abs)/docker/Dockerfile"
+  echo "Building image $(image_name) from ${GIT_REPO_DIR}/docker/Dockerfile"
   docker build \
     --build-arg GIT_COMMIT_SHA="$(git_commit_sha)" \
     --compress \
-    --file "$(src_dir_abs)/docker/Dockerfile" \
+    --file "${GIT_REPO_DIR}/docker/Dockerfile" \
     --tag "$(image_name)" \
-    "$(src_dir_abs)/docker"
-}
-
-#- - - - - - - - - - - - - - - - - - - - - - -
-src_dir_abs()
-{
-  # docker volume-mounts cannot be relative
-  echo $(cd ${SRC_DIR} && pwd)
+    "${GIT_REPO_DIR}/docker"
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - -
@@ -224,7 +213,7 @@ image_name()
 {
   docker run \
     --rm \
-    --volume "$(src_dir_abs):/data:ro" \
+    --volume "${GIT_REPO_DIR}:/data:ro" \
     cyberdojofoundation/image_namer
 }
 
@@ -252,7 +241,7 @@ testing_myself()
 # - - - - - - - - - - - - - - - - - - - - - - -
 has_start_point()
 {
-  [ -d "$(src_dir_abs)/start_point" ]
+  [ -d "${GIT_REPO_DIR}/start_point" ]
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
@@ -337,8 +326,9 @@ lsp_image_name()
 build_lsp_image()
 {
   local -r name=$(lsp_image_name)
+  local -r tag="$(git_commit_tag)"
   echo "Building ${name}"
-  "$(cyber_dojo)" start-point build "${name}" --languages "${GIT_REPO_URL}"
+  "$(cyber_dojo)" start-point build "${name}" --languages "${tag}@${GIT_REPO_DIR}"
 }
 
 remove_lsp_image()
@@ -377,8 +367,6 @@ remove_lsp_container()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
-readonly READY_FILENAME='/tmp/curl-ready-output'
-
 wait_until_ready()
 {
   local -r name="${1}"
@@ -387,7 +375,7 @@ wait_until_ready()
   printf "Waiting until ${name} is ready"
   for _ in $(seq ${max_tries})
   do
-    if ready ${port} ; then
+    if ready $(ip_address) ${port} ; then
       printf '.OK\n'
       return
     else
@@ -405,18 +393,29 @@ wait_until_ready()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
+ip_address()
+{
+  if [ -n "${DOCKER_MACHINE_NAME}" ]; then
+    docker-machine ip ${DOCKER_MACHINE_NAME}
+  else
+    echo localhost
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - -
 ready()
 {
-  local -r port="${1}"
+  local -r ip_address="${1}"
+  local -r port="${2}"
   local -r path=ready?
+  rm -f "$(ready_filename)"
   local -r curl_cmd="curl \
-    --output ${READY_FILENAME} \
+    --output $(ready_filename) \
     --silent \
     --fail \
     --data {} \
-    -X GET http://${IP_ADDRESS}:${port}/${path}"
-  rm -f "${READY_FILENAME}"
-  if ${curl_cmd} && [ "$(cat "${READY_FILENAME}")" = '{"ready?":true}' ]; then
+    -X GET http://$(ip_address):${port}/${path}"
+  if ${curl_cmd} && [ "$(cat "$(ready_filename)")" = '{"ready?":true}' ]; then
     true
   else
     false
@@ -424,9 +423,15 @@ ready()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
+ready_filename()
+{
+  echo /tmp/curl-ready-output
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - -
 # check red->amber->green progression of '6 * 9'
 # Works via a volume-mount (and not via a git-clone) so
-#   o) uncommitted changes in SRC_DIR will be seen.
+#   o) uncommitted changes in GIT_REPO_DIR will be seen.
 #   o) start_point/options.json can be used if needed.
 # - - - - - - - - - - - - - - - - - - - - - - -
 assert_traffic_light()
@@ -434,7 +439,7 @@ assert_traffic_light()
   local -r colour="${1}" # red|amber|green
   docker run \
     --env NO_PROMETHEUS=true \
-    --env SRC_DIR=$(src_dir_abs) \
+    --env SRC_DIR=${GIT_REPO_DIR} \
     --init \
     --name traffic-light \
     --network $(docker_network_name) \
@@ -443,14 +448,15 @@ assert_traffic_light()
     --rm \
     --tmpfs /tmp \
     --user nobody \
-    --volume $(src_dir_abs):$(src_dir_abs):ro \
+    --volume ${GIT_REPO_DIR}:${GIT_REPO_DIR}:ro \
       cyberdojofoundation/image_hiker:latest "${colour}"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 check_version()
 {
-  "${SRC_DIR}/check_version.sh"
+  echo 'No ${GIT_REPO_DIR}/start_point/ dir so assuming base-language repo'
+  "${GIT_REPO_DIR}/check_version.sh"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
@@ -483,15 +489,14 @@ export $(versioner_env_vars)
 exit_zero_if_show_help ${*}
 exit_non_zero_unless_git_installed
 exit_non_zero_unless_docker_installed
-exit_non_zero_unless_good_SRC_DIR ${*}
-set_git_repo_url
+exit_non_zero_unless_good_GIT_REPO_DIR ${*}
+set_git_repo_dir ${*}
 build_cdl_image
 tag_cdl_image_with_commit_sha
 
 if has_start_point; then
   check_red_amber_green
 else
-  echo 'No ${SRC_DIR}/start_point/ dir so assuming base-language repo'
   check_version
 fi
 
