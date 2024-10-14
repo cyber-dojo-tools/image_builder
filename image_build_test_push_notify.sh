da405540dec88d7331675969df1e0f18aca858ed
Faye <faye@kosli.com>
2024-10-14 16:48:38 +0200

    Use separate docker tag commands

--- image_build_test_push_notify.sh
+++ image_build_test_push_notify.sh
@@ -239,8 +239,10 @@ check_version()
 # - - - - - - - - - - - - - - - - - - - - - - -
 tag_cdl_image_with_commit_sha()
 {
-  docker tag $(image_name) ghcr.io/$(image_name):$(git_commit_tag) ghcr.io/$(image_name):latest
+  docker tag $(image_name) ghcr.io/$(image_name):$(git_commit_tag)
   echo "Successfully tagged to ghcr.io/$(image_name):$(git_commit_tag)"
+  docker tag $(image_name) ghcr.io/$(image_name):latest
+  echo "Successfully tagged to ghcr.io/$(image_name):latest"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
